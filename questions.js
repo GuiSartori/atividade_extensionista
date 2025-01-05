6cba7bafc04a7e47939010adc47b1a1592d191f7
GuiSartori <guilherme.sartori@live.com>
2025-01-05 13:19:14 -0300

    Questões de arte

---
+++ questions.js
@@ -0,0 +1,112 @@
+const questions = [
+    {
+        question: "O que é um vídeo?",
+        alternatives: {
+            A: "Uma sequência de imagens ou desenhos estáticos que, quando reproduzidas a uma certa velocidade, dão a sensação de movimento.",
+            B: "Uma animação realizada em computador.",
+            C: "Uma coleção de fotos.",
+            D: "Um documento escrito sobre cinema."
+        },
+        correctAnswer: "A",
+        feedback: "O correto seria: Uma sequência de imagens ou desenhos estáticos que, quando reproduzidas a uma certa velocidade, dão a sensação de movimento. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "Qual é a taxa de quadros por segundo padrão na indústria do cinema?",
+        alternatives: {
+            A: "24 quadros por minuto.",
+            B: "24 quadros por segundo.",
+            C: "30 quadros por segundo.",
+            D: "60 quadros por segundo."
+        },
+        correctAnswer: "B",
+        feedback: "O correto seria: 24 quadros por segundo. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "Qual nome se dá ao tipo de animação feita a partir da junção de várias fotos de objetos e personagens em posições sequenciais?",
+        alternatives: {
+            A: "Animação 3D.",
+            B: "Desenho animado.",
+            C: "Animação por computador.",
+            D: "Stop-motion."
+        },
+        correctAnswer: "D",
+        feedback: "O correto seria: Stop-motion. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "O que é história, no contexto do cinema?",
+        alternatives: {
+            A: "A narrativa completa de um filme, incluindo todos os detalhes e como os fatos são contados.",
+            B: "O enredo básico ou a sequência de eventos de um filme.",
+            C: "A descrição dos personagens.",
+            D: "O diálogo entre os personagens."
+        },
+        correctAnswer: "A",
+        feedback: "O correto seria: A narrativa completa de um filme, incluindo todos os detalhes e como os fatos são contados. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "O que é narrativa, no contexto do cinema?",
+        alternatives: {
+            A: "A sequência de eventos na ordem em que ocorrem.",
+            B: "O resumo do filme.",
+            C: "A maneira como a história é estruturada e contada.",
+            D: "A descrição visual das cenas."
+        },
+        correctAnswer: "C",
+        feedback: "O correto seria: A maneira como a história é estruturada e contada. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "Quais são os elementos que compõem um roteiro de cinema?",
+        alternatives: {
+            A: "Atores, cenários, luzes e som.",
+            B: "Cabeçalho, ação, diálogo e transições.",
+            C: "Direção, produção, edição e distribuição.",
+            D: "Música, efeitos sonoros, trilha sonora e locação."
+        },
+        correctAnswer: "B",
+        feedback: "O correto seria: Cabeçalho, ação, diálogo e transições. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "O que é um cabeçalho de cena em um roteiro de cinema?",
+        alternatives: {
+            A: "A descrição detalhada do cenário.",
+            B: "A linha que informa a localização e o tempo da cena.",
+            C: "A fala de um personagem.",
+            D: "A instrução para os movimentos de câmera."
+        },
+        correctAnswer: "B",
+        feedback: "O correto seria: A linha que informa a localização e o tempo da cena. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "O que é uma ação em um roteiro de cinema?",
+        alternatives: {
+            A: "A descrição dos diálogos.",
+            B: "A transição entre cenas.",
+            C: "A descrição das atividades e movimentos dos personagens.",
+            D: "A sinopse do filme."
+        },
+        correctAnswer: "C",
+        feedback: "O correto seria: A descrição das atividades e movimentos dos personagens. Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "Qual dessas opções representa a maneira correta de escrever uma linha de diálogo em um roteiro de cinema?",
+        alternatives: {
+            A: "BRUNA - Meu Deus! Que horas são?",
+            B: "Bruna diz: \"Meu Deus! Que horas são?\"",
+            C: "(BRUNA) - Meu Deus! Que horas são?",
+            D: "- Meu Deus! Que horas são? – disse Bruna, surpresa."
+        },
+        correctAnswer: "A",
+        feedback: "O correto seria: BRUNA - Meu Deus! Que horas são? Para saber mais, <a href='#'>acesse o link</a>."
+    },
+    {
+        question: "Qual é a função do uso de parênteses em uma linha de diálogo no roteiro?",
+        alternatives: {
+            A: "Mostrar a fala de outro personagem.",
+            B: "Indicar o cenário.",
+            C: "Marcar a passagem do tempo.",
+            D: "Indicar uma ação ou emoção de um personagem, ou a direção de sua fala."
+        },
+        correctAnswer: "D",
+        feedback: "O correto seria: Indicar uma ação ou emoção de um personagem, ou a direção de sua fala. Para saber mais, <a href='#'>acesse o link</a>."
+    }
+];
