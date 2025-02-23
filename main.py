200c06d1227b7341c34bd8b529f6a0110674f762
GuiSartori <guilherme.sartori@live.com>
2025-02-23 17:02:44 -0300

    primeiro commit de relatório

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from utils.data_functions import *
+
+# Exemplo de uso
+data_folder = 'data'
+output_folder = os.path.join(data_folder, 'aglomerado')
+aglomerar_resultados(data_folder, output_folder)
+
+# Gerar relatório para uma turma específica
+file_path = os.path.join(output_folder, 'turma_4D.xlsx')
+
+resultado_turma(file_path)
+
+# Gerar relatório para uma turma específica
+file_path = os.path.join(output_folder, 'turma_5D.xlsx')
+
+resultado_turma(file_path)
